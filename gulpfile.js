0bc4ee746e0cca7b9b9ea028858b57ae85ded027
Madeleine <madeleinemsandersson@gmail.com>
2019-04-13 12:03:56 +0200

    Updated gulpfile, added gulp-htmlmin

--- gulpfile.js
+++ gulpfile.js
@@ -4,14 +4,15 @@ const gulp = require("gulp"),
   autoprefixer = require("autoprefixer"),
   cssnano = require("cssnano"),
   babel = require("gulp-babel"),
-  uglify = require("gulp-uglify");
+  uglify = require("gulp-uglify"),
+  htmlmin = require("gulp-htmlmin");
 
 function styles() {
   return gulp
     .src("src/sass/**/*.scss")
     .pipe(sass().on("error", sass.logError))
     .pipe(postcss([autoprefixer(), cssnano()]))
-    .pipe(gulp.dest("src/css"));
+    .pipe(gulp.dest("build/css"));
 }
 
 function watch() {
@@ -26,6 +27,30 @@ function scripts() {
     .pipe(gulp.dest("build/js"));
 }
 
+function pages() {
+  return gulp
+    .src(["./src/**/*.html"])
+    .pipe(
+      htmlmin({
+        collapseWhitespace: true
+      })
+    )
+    .pipe(gulp.dest("build"));
+}
+
+function images() {
+  return gulp.src("src/img/*.*").pipe(gulp.dest("build/img"));
+}
+
+function build(done) {
+  const runTasks = gulp.series("scripts", "styles", "pages", "images");
+  runTasks();
+  done();
+}
+
 exports.styles = styles;
 exports.watch = watch;
 exports.scripts = scripts;
+exports.pages = pages;
+exports.images = images;
+exports.build = build;
